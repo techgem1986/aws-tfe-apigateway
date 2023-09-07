b12622ad28a34fc18757a62a738dbf5ddb0021cc
Sandipan Sarkar <work.sandipan@gmail.com>
2023-09-07 09:51:00 +0530

    https://techgem1986-jira.atlassian.net/browse/TG001-4

--- sportbuzz-api-gateway.tf
+++ sportbuzz-api-gateway.tf
@@ -2,58 +2,64 @@ data aws_lambda_function "sportbuzz-users-statistics-python-lambda"{
   function_name = "sportbuzz-users-statistics-python-lambda"
 }
 
-resource "aws_apigatewayv2_api" "sportbuzz-api-gateway" {
+resource "aws_api_gateway_rest_api" "sportbuzz-api-gateway" {
   name          = "sportbuzz-api-gateway"
   protocol_type = "HTTP"
 }
 
-resource "aws_cloudwatch_log_group" "sportbuzz-api-gateway-log" {
-  name = "/aws/api-gateway/${aws_apigatewayv2_api.sportbuzz-api-gateway.name}"
-  retention_in_days = 30
+resource "aws_api_gateway_resource" "sportbuzz" {
+  depends_on = ["aws_api_gateway_rest_api.sportbuzz-api-gateway"]
+
+  rest_api_id = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
+  parent_id   = aws_api_gateway_rest_api.sportbuzz-api-gateway.root_resource_id
+  path_part   = "sportbuzz"
 }
 
-resource "aws_apigatewayv2_stage" "sportbuzz-api-gateway_stage" {
-  api_id = aws_apigatewayv2_api.sportbuzz-api-gateway.id
-  name        = "sportbuzz"
-  auto_deploy = true
+resource "aws_api_gateway_resource" "users" {
+  depends_on = ["aws_api_gateway_rest_api.sportbuzz-api-gateway", "aws_api_gateway_resource.sportbuzz"]
 
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.sportbuzz-api-gateway-log.arn
+  rest_api_id = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
+  parent_id   = aws_api_gateway_resource.sportbuzz.id
+  path_part   = "users"
+}
 
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-    }
-    )
+resource "aws_api_gateway_resource" "statistics" {
+  depends_on = ["aws_api_gateway_rest_api.sportbuzz-api-gateway", "aws_api_gateway_resource.users"]
+
+  rest_api_id = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
+  parent_id   = aws_api_gateway_resource.users.id
+  path_part   = "statistics"
+}
+
+resource "aws_api_gateway_method" "users-statistics" {
+  depends_on = ["aws_api_gateway_resource.statistics"]
+
+  rest_api_id           = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
+  resource_id           = aws_api_gateway_resource.statistics.id
+  http_method           = "ANY"
+  authorization         = "NONE"
+
+  request_parameters = {
+    "method.request.querystring.uid" = true
   }
 }
 
-resource "aws_apigatewayv2_integration" "sportbuzz-users-statistics-api-gateway-integration" {
-  api_id = aws_apigatewayv2_api.sportbuzz-api-gateway.id
+resource "aws_api_gateway_integration" "users-statistics" {
+  depends_on = ["aws_api_gateway_method.users-statistics"]
 
-  integration_uri    = data.aws_lambda_function.sportbuzz-users-statistics-python-lambda.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
+  rest_api_id             = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
+  resource_id             = aws_api_gateway_resource.statistics.id
+  http_method             = aws_api_gateway_method.users-statistics.http_method
+  type                    = "AWS"
+  uri                     = data.aws_lambda_function.sportbuzz-users-statistics-python-lambda.invoke_arn
+  integration_http_method = "GET"
 }
 
-resource "aws_apigatewayv2_route" "sportbuzz-users-statistics-api-gateway-route" {
-  api_id = aws_apigatewayv2_api.sportbuzz-api-gateway.id
-  route_key = "GET /getUser"
-  target    = "integrations/${aws_apigatewayv2_integration.sportbuzz-users-statistics-api-gateway-integration.id}"
-}
 
 resource "aws_lambda_permission" "sportbuzz-users-statistics-api-gateway-lambda-permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
   function_name = data.aws_lambda_function.sportbuzz-users-statistics-python-lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "${aws_apigatewayv2_api.sportbuzz-api-gateway.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.sportbuzz-api-gateway.execution_arn}/*/*"
 }
