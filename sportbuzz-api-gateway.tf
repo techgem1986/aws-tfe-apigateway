5077e9718740296ab69b373f0f9ad6982cee6f74
Sandipan Sarkar <work.sandipan@gmail.com>
2023-09-07 09:55:17 +0530

    https://techgem1986-jira.atlassian.net/browse/TG001-4

--- sportbuzz-api-gateway.tf
+++ sportbuzz-api-gateway.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_rest_api" "sportbuzz-api-gateway" {
 }
 
 resource "aws_api_gateway_resource" "sportbuzz" {
-  depends_on = ["aws_api_gateway_rest_api.sportbuzz-api-gateway"]
+  depends_on = [aws_api_gateway_rest_api.sportbuzz-api-gateway]
 
   rest_api_id = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
   parent_id   = aws_api_gateway_rest_api.sportbuzz-api-gateway.root_resource_id
@@ -16,7 +16,7 @@ resource "aws_api_gateway_resource" "sportbuzz" {
 }
 
 resource "aws_api_gateway_resource" "users" {
-  depends_on = ["aws_api_gateway_rest_api.sportbuzz-api-gateway", "aws_api_gateway_resource.sportbuzz"]
+  depends_on = [aws_api_gateway_rest_api.sportbuzz-api-gateway, aws_api_gateway_resource.sportbuzz]
 
   rest_api_id = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
   parent_id   = aws_api_gateway_resource.sportbuzz.id
@@ -24,7 +24,7 @@ resource "aws_api_gateway_resource" "users" {
 }
 
 resource "aws_api_gateway_resource" "statistics" {
-  depends_on = ["aws_api_gateway_rest_api.sportbuzz-api-gateway", "aws_api_gateway_resource.users"]
+  depends_on = [aws_api_gateway_rest_api.sportbuzz-api-gateway, aws_api_gateway_resource.users]
 
   rest_api_id = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
   parent_id   = aws_api_gateway_resource.users.id
@@ -32,7 +32,7 @@ resource "aws_api_gateway_resource" "statistics" {
 }
 
 resource "aws_api_gateway_method" "users-statistics" {
-  depends_on = ["aws_api_gateway_resource.statistics"]
+  depends_on = [aws_api_gateway_resource.statistics]
 
   rest_api_id           = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
   resource_id           = aws_api_gateway_resource.statistics.id
@@ -45,7 +45,7 @@ resource "aws_api_gateway_method" "users-statistics" {
 }
 
 resource "aws_api_gateway_integration" "users-statistics" {
-  depends_on = ["aws_api_gateway_method.users-statistics"]
+  depends_on = [aws_api_gateway_method.users-statistics]
 
   rest_api_id             = aws_api_gateway_rest_api.sportbuzz-api-gateway.id
   resource_id             = aws_api_gateway_resource.statistics.id
